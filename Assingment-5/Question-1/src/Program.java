9052ae84b22f3a06d02a584641fca1efab062427
Ajaykadu <kaduajay1122@gmail.com>
2024-09-17 23:07:19 +0530

    Assingment-5/Q1

---
+++ Assingment-5/Question-1/src/Program.java
@@ -0,0 +1,16 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+	
+		String str = "This is the Sample String";
+		 
+		StringBuilder sb = new StringBuilder(str);
+		
+		sb.reverse().toString();
+		
+		System.out.println(sb);
+		
+	}
+
+}
